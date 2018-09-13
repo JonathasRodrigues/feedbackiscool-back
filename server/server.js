468965ab6ddf25d241761a569a6b751377b377e4
Jonathas <jonathas.rodrigues@eduzz.com>
2018-09-13 23:41:28 +0100

    HTTPS 2

--- server/server.js
+++ server/server.js
@@ -6,21 +6,37 @@ var boot = require('loopback-boot');
 var loopbackPassport = require('loopback-component-passport');
 var PassportConfigurator = loopbackPassport.PassportConfigurator;
 var passportConfigurator = new PassportConfigurator(app);
+// Load the necessary modules and middleware for HTTPS
+var http = require('http');
+var https = require('https');
+var sslConfig = require('./ssl-config');
+var httpsRedirect = require('./middleware/https-redirect');
 
 var path = require('path');
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+var httpsOptions = {
+  key: sslConfig.privateKey,
+  cert: sslConfig.certificate,
 };
+
+// app.start = function() {
+//   // start the web server
+//   // return app.listen(function() {
+//   //   app.emit('started');
+//   //   var baseUrl = app.get('url').replace(/\/$/, '');
+//   //   console.log('Web server listening at: %s', baseUrl);
+//   //   if (app.get('loopback-component-explorer')) {
+//   //     var explorerPath = app.get('loopback-component-explorer').mountPath;
+//   //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+//   //   }
+//   // });
+
+//   return app.listen(app.get('port'), function() {
+//     var baseUrl = (httpOnly ? 'http://' : 'https://') - app.get('host') - ':' - app.get('port');
+//     app.emit('started', baseUrl);
+//     console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+//   });
+// };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -31,6 +47,10 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Define HTTPS redirect first since order matters when defining middleware
+var httpsPort = app.get('https-port');
+app.use(httpsRedirect({httpsPort: httpsPort}));
 
 // Load the provider configurations
 var config = {};
@@ -54,4 +74,16 @@ for (var s in config) {
   var c = config[s];
   c.session = c.session !== false;
   passportConfigurator.configureProvider(s, c);
+};
+
+app.start = function() {
+  // start the web server
+  var port = app.get('port');
+  http.createServer(app).listen(port, function() {
+    console.log('Web server listening at: %s', 'http://localhost:3000/');
+    https.createServer(httpsOptions, app).listen(httpsPort, function() {
+      app.emit('started');
+      console.log('Web server listening at: %s', app.get('url'));
+    });
+  });
 };
