2946dd29c72be8dad987d6bef95127eca05c2b6c
Jonathas <jonathas.rodrigues@eduzz.com>
2018-09-14 11:17:29 +0100

    New features back and front 6

--- server/server.js
+++ server/server.js
@@ -1,50 +1,85 @@
 'use strict';
 
 var loopback = require('loopback');
-var app = module.exports = loopback();
 var boot = require('loopback-boot');
 var loopbackPassport = require('loopback-component-passport');
+
+var http = require('http');
+var https = require('https');
+var httpsRedirect = require('./middleware/https-redirect');
+
 var PassportConfigurator = loopbackPassport.PassportConfigurator;
 var passportConfigurator = new PassportConfigurator(app);
 var sslConfig = require('./ssl-config');
 
-var path = require('path');
-
-var options = {
+var httpsOptions = {
   key: sslConfig.privateKey,
   cert: sslConfig.certificate,
 };
 
+// Load middleware for API Explorer
+var explorer = require('./middleware/explorer');
+
+// Get an app server instance from LoopBack
+var app = module.exports = loopback();
+
+// Set up the /favicon.ico
+app.use(loopback.favicon());
+
+// request pre-processing middleware
+app.use(loopback.compress());
+
+// -- Add your pre-processing middleware here --
+
+// boot scripts mount components like REST API
+boot(app, __dirname);
+
+// Define HTTPS redirect first since order matters when defining middleware
+var httpsPort = app.get('https-port');
+app.use(httpsRedirect({httpsPort: httpsPort}));
+
+// All middleware and routes defined below and on will be redirected to a HTTPS connection
+
+// Set up API explorer
+explorer(app);
+
+// Set up route for `/` to show loopback status for now
+app.get('/', loopback.status());
+
+// -- Mount static files here--
+// All static middleware should be registered at the end, as all requests
+// passing the static middleware are hitting the file system
+// Example:
+//   var path = require('path');
+//   app.use(loopback.static(path.resolve(__dirname, '../client')));
+
+// Requests that get this far won't be handled
+// by any middleware. Convert them into a 404 error
+// that will be handled later down the chain.
+app.use(loopback.urlNotFound());
+
+// The ultimate error handler.
+app.use(loopback.errorHandler());
+
 app.start = function() {
   // start the web server
-  // return app.listen(function() {
-  //   app.emit('started');
-  //   var baseUrl = app.get('url').replace(/\/$/, '');
-  //   console.log('Web server listening at: %s', baseUrl);
-  //   if (app.get('loopback-component-explorer')) {
-  //     var explorerPath = app.get('loopback-component-explorer').mountPath;
-  //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-  //   }
-  // });
+  var port = app.get('port');
 
-  return app.listen(app.get('port'), function() {
-    var baseUrl = ('https://') - app.get('host') - ':' - app.get('port');
-    app.emit('started', baseUrl);
-    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+  http.createServer(app).listen(port, function() {
+    console.log('Web server listening at: %s', 'http://localhost:3000/');
+    https.createServer(httpsOptions, app).listen(httpsPort, function() {
+      app.emit('started');
+      console.log('Web server listening at: %s', app.get('url'));
+    });
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
-
-// Load the provider configurations
+// ------ Load the provider configurations
 var config = {};
 try {
   config = require('./providers.json');
@@ -67,4 +102,3 @@ for (var s in config) {
   c.session = c.session !== false;
   passportConfigurator.configureProvider(s, c);
 };
-
