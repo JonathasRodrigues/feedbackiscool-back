141337b550cdd4994e5aaedcb17f5c04ceb08d91
Jonathas <jonathas.rodrigues@eduzz.com>
2018-09-14 10:35:30 +0100

    New features back and front 4

--- server/server.js
+++ server/server.js
@@ -4,29 +4,45 @@ var loopback = require('loopback');
 var app = module.exports = loopback();
 var boot = require('loopback-boot');
 var loopbackPassport = require('loopback-component-passport');
-var http = require('http');
-var https = require('https');
 var PassportConfigurator = loopbackPassport.PassportConfigurator;
 var passportConfigurator = new PassportConfigurator(app);
 var sslConfig = require('./ssl-config');
 
 var path = require('path');
 
-// app.start = function() {
-//   return app.listen(function() {
-//     app.emit('started');
-//     var baseUrl = app.get('url').replace(/\/$/, '');
-//     console.log('Web server listening at: %s', baseUrl);
-//     if (app.get('loopback-component-explorer')) {
-//       var explorerPath = app.get('loopback-component-explorer').mountPath;
-//       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-//     }
-//   });
-// };
+var options = {
+  key: sslConfig.privateKey,
+  cert: sslConfig.certificate,
+};
+
+app.start = function() {
+  // start the web server
+  // return app.listen(function() {
+  //   app.emit('started');
+  //   var baseUrl = app.get('url').replace(/\/$/, '');
+  //   console.log('Web server listening at: %s', baseUrl);
+  //   if (app.get('loopback-component-explorer')) {
+  //     var explorerPath = app.get('loopback-component-explorer').mountPath;
+  //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  //   }
+  // });
+
+  return app.listen(app.get('port'), function() {
+    var baseUrl = ('https://') - app.get('host') - ':' - app.get('port');
+    app.emit('started', baseUrl);
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+  });
+};
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname);
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
 
 // Load the provider configurations
 var config = {};
@@ -52,33 +68,3 @@ for (var s in config) {
   passportConfigurator.configureProvider(s, c);
 };
 
-app.start = function(httpOnly) {
-  if (httpOnly === undefined) {
-    httpOnly = process.env.HTTP;
-  }
-  var server = null;
-  if (!httpOnly) {
-    var options = {
-      key: sslConfig.privateKey,
-      cert: sslConfig.certificate,
-    };
-    server = https.createServer(options, app);
-  } else {
-    server = http.createServer(app);
-  }
-  server.listen(app.get('port'), function() {
-    var baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
-    app.emit('started', baseUrl);
-    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-  return server;
-};
-
-// start the server if `$ node server.js`
-if (require.main === module) {
-  app.start();
-}
