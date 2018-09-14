3d23c71d54c36f6575d09209664fd1c545ce6447
Jonathas <jonathas.rodrigues@eduzz.com>
2018-09-14 17:29:06 +0100

    HTTPS ajuste 2

--- server/server.js
+++ server/server.js
@@ -25,31 +25,6 @@ var app = module.exports = loopback();
 //   });
 // };
 
-app.start = function(httpOnly) {
-  if (httpOnly === undefined) {
-    httpOnly = process.env.HTTP;
-  }
-  var server = null;
-  if (!httpOnly) {
-    var options = {
-      key: sslConfig.privateKey,
-      cert: sslConfig.certificate,
-    };
-    server = https.createServer(options, app);
-  } else {
-    server = http.createServer(app);
-  }
-  return server.listen(app.get('port'), function() {
-    var baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
-    app.emit('started', baseUrl);
-    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 // boot(app, __dirname, function(err) {
