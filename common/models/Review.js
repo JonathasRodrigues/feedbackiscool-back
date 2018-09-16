6ed55a0f433b5d8d8be4e878157a8c6e21922ecd
Jonathas <jonathas.rodrigues@eduzz.com>
2018-09-16 23:17:12 +0100

    Add google ad and created date review

--- common/models/Review.js
+++ common/models/Review.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Review) {
-
+  // Model is the model class
+  // options is an object containing the config properties from model definition
+  Review.defineProperty('created', {type: Date, default: '$now'});
 };
