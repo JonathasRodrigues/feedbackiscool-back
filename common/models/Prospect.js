dea95f1f1192378652bd5c889dd20eddbf0699a4
Jonathas <jonathas.rodrigues@eduzz.com>
2019-03-20 15:18:33 +0000

    removed setAcessToken

--- common/models/Prospect.js
+++ common/models/Prospect.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Prospect) {
-
+    // Model is the model class
+  // options is an object containing the config properties from model definition
+  Prospect.defineProperty('created', {type: Date, default: '$now'});
 };
